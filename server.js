187beed6f4faed5cf7667140bfbd9e234b42ea19
Darko Stepanovic <dare.stepanovic@gmail.com>
2020-10-05 20:12:07 +0200

    Add thank you page, bugfixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('dist', { extensions: ['html', 'htm']}));
 
 app.get('/', (req, res) => {
     res.send('Works!');
